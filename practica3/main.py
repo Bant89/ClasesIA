47c1ef475b71966fd78976f002864cb4f1504c42
Bant89 <bant8009@gmail.com>
2020-06-07 21:40:52 -0400

    finish part e

--- practica3/main.py
+++ practica3/main.py
@@ -165,15 +165,94 @@ def d(print_maze=True):
 
     print(path)
 
+# For Section E
 
-# For section E
+
+# Maze size
+N = 8
+
+# A utility function to print solution matrix sol
 
 
-def main():
+def printSolution(sol):
+
+    for i in sol:
+        for j in i:
+            print(str(j) + " ", end="")
+        print("")
+
+# A utility function to check if x, y is valid
+# index for N * N Maze
+
+
+def isSafe(maze, x, y):
+
+    if x >= 0 and x < N and y >= 0 and y < N and maze[x][y] == 1:
+        return True
+
+    return False
+
+
+def cost_to_goal(x, y):
+    return x ** 2 + y ** 2
+
+
+def solveMaze(maze):
+
+    # Creating a 8 * 8 2-D list
+    sol = [[0 for j in range(8)] for i in range(8)]
+
+    if solveMazeUtil(maze, 0, 0, sol) == False:
+        print("Solution doesn't exist")
+        return False
+
+    printSolution(sol)
+    return True
+
+
+def solveMazeUtil(maze, x, y, sol):
+
+    if x == N - 1 and y == N - 1 and maze[x][y] == 1:
+        sol[x][y] = 1
+        return True
+
+    if isSafe(maze, x, y) == True:
+        # mark x, y as part of solution path
+        sol[x][y] = 1
+
+        # Move forward in x direction
+        if solveMazeUtil(maze, x + 1, y, sol) == True:
+            return True
+
+        # If moving in x direction doesn't give solution
+        # then Move down in y direction
+        if solveMazeUtil(maze, x, y + 1, sol) == True:
+            return True
+
+        # If none of the above movements work then
+        # BACKTRACK: unmark x, y as part of solution path
+        if cost_to_goal(x, y) < sol[x][y]:
+            sol[x][y] = cost_to_goal(x, y)
+        else:
+            sol[x][y] = 0
+        return False
+
+
+if __name__ == "__main__":
+    # Initialising the maze
 
     # For section D
     d(True)
     # Path found [(5, 3), (6, 3), (6, 2), (6, 1), (5, 1), (5, 0), (4, 0), (3, 0), (3, 1), (2, 1), (2, 2)]
+    maze = [[0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0], ]
 
-
-main()
+    # For section E
+    solveMaze(maze)
+    # Path found [(5,3), (6, 3),(6, 2),(6, 1),(5, 1),(5, 0),(4, 0),(3, 0),(3, 1),(2, 1),(2,2)]
